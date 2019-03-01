eea4e87033d13c5f6b140f82db2a9eb28e430fde
Ralph Slooten <axllent@gmail.com>
2019-03-02 12:26:40 +1300

    Add database update functionality
    Squashed commit of the following:
    
    commit 60ec118f9dea5a4b44519f25b058a4656f6cdbfb
    Author: Ralph Slooten <axllent@gmail.com>
    Date:   Sat Mar 2 12:26:14 2019 +1300
    
        Add update functionality
    
    commit a8ba33b916017ec7ebcd89091407f563c0d178dc
    Author: Ralph Slooten <axllent@gmail.com>
    Date:   Sat Mar 2 10:25:45 2019 +1300
    
        Use correct indenting
    
    commit 205f7ebaef691425cfdaa70591ab00b745541060
    Author: Ralph Slooten <axllent@gmail.com>
    Date:   Sat Mar 2 10:02:01 2019 +1300
    
        Start updater

--- geoiplookup.go
+++ geoiplookup.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "github.com/oschwald/geoip2-golang"
-    "net"
-    "os"
-    "path"
-    "strings"
+	"archive/tar"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"github.com/oschwald/geoip2-golang"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 // global flags
@@ -20,17 +26,21 @@ var data_dir = flag.String("d", "/usr/share/GeoIP", "database directory or file"
  * Main function
  */
 func main() {
-    showhelp := flag.Bool("h", false, "show help")
-    flag.Parse()
+	showhelp := flag.Bool("h", false, "show help")
+	flag.Parse()
 
-    if len(flag.Args()) != 1 || *showhelp {
-        Usage()
-        os.Exit(1)
-    }
+	if len(flag.Args()) != 1 || *showhelp {
+		Usage()
+		os.Exit(1)
+	}
 
-    lookup := flag.Args()[0]
+	lookup := flag.Args()[0]
 
-    Lookup(lookup)
+	if lookup == "update" {
+		UpdateGeoLite2Country()
+	} else {
+		Lookup(lookup)
+	}
 }
 
 /**
@@ -38,95 +48,211 @@ func main() {
  */
 func Lookup(lookup string) {
 
-    var ciso     string
-    var cname    string
-    var mmdb     string
-    var output []string
-    var response string
-    var ipraw    string
+	var ciso string
+	var cname string
+	var mmdb string
+	var output []string
+	var response string
+	var ipraw string
 
-    // convert to ip if hostname
-    addresses, err := net.LookupHost(lookup)
+	// convert to ip if hostname
+	addresses, err := net.LookupHost(lookup)
 
-    if len(addresses) > 0 {
-        Debug(fmt.Sprintf("Ip search for: %s", addresses[0]))
-        ipraw = addresses[0];
-    } else {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
+	if len(addresses) > 0 {
+		Debug(fmt.Sprintf("Ip search for: %s", addresses[0]))
+		ipraw = addresses[0]
+	} else {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 
-    fi, err := os.Stat(*data_dir)
-    if err != nil {
-        fmt.Println("Error: File does not exist", *data_dir)
-        os.Exit(1)
-    }
+	fi, err := os.Stat(*data_dir)
+	if err != nil {
+		fmt.Println("Error: Directory does not exist", *data_dir)
+		os.Exit(1)
+	}
 
-    switch mode := fi.Mode(); {
-        case mode.IsDir():
-            mmdb = path.Join(*data_dir, "GeoLite2-Country.mmdb")
-        case mode.IsRegular():
-            mmdb = *data_dir
-    }
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		mmdb = path.Join(*data_dir, "GeoLite2-Country.mmdb")
+	case mode.IsRegular():
+		mmdb = *data_dir
+	}
 
-    Debug(fmt.Sprintf("Opening %s", mmdb))
+	Debug(fmt.Sprintf("Opening %s", mmdb))
 
-    db, err := geoip2.Open(mmdb)
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
-    defer db.Close()
+	db, err := geoip2.Open(mmdb)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	defer db.Close()
 
-    ip := net.ParseIP(ipraw)
+	ip := net.ParseIP(ipraw)
 
-    record, err := db.Country(ip)
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
+	record, err := db.Country(ip)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 
-    if record.Traits.IsAnonymousProxy {
-        Debug("Anonymous IP detected")
-        ciso  = "A1"
-        cname = "Anonymous Proxy"
-    } else {
-        ciso  = record.Country.IsoCode
-        cname = record.Country.Names["en"]
-    }
+	if record.Traits.IsAnonymousProxy {
+		Debug("Anonymous IP detected")
+		ciso = "A1"
+		cname = "Anonymous Proxy"
+	} else {
+		ciso = record.Country.IsoCode
+		cname = record.Country.Names["en"]
+	}
 
-    if ( *country || *iso ) {
-        if *iso && ciso != "" {
-            output = append(output, ciso)
-        }
-        if *country && cname != "" {
-            output = append(output, cname)
-        }
-        response = fmt.Sprintf(strings.Join(output, ", "))
-    } else {
-        if ciso == "" {
-            response = "GeoIP Country Edition: IP Address not found"
-        } else {
-            response = fmt.Sprintf("GeoIP Country Edition: %s, %s", ciso, cname)
-        }
-    }
+	if *country || *iso {
+		if *iso && ciso != "" {
+			output = append(output, ciso)
+		}
+		if *country && cname != "" {
+			output = append(output, cname)
+		}
+		response = fmt.Sprintf(strings.Join(output, ", "))
+	} else {
+		if ciso == "" {
+			response = "GeoIP Country Edition: IP Address not found"
+		} else {
+			response = fmt.Sprintf("GeoIP Country Edition: %s, %s", ciso, cname)
+		}
+	}
 
-    fmt.Println(response)
+	fmt.Println(response)
+}
+
+func UpdateGeoLite2Country() {
+	url := "https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz"
+
+	Debug("Updating GeoLite2-Country.mmdb")
+
+	// check the output directory is writeable
+	if _, err := os.Stat(*data_dir); os.IsNotExist(err) {
+		os.MkdirAll(*data_dir, os.ModePerm)
+	}
+
+	_, err := os.Stat(*data_dir)
+	if err != nil {
+		fmt.Println("Error: Cannot create", *data_dir)
+		os.Exit(1)
+	}
+
+	if err := DownloadFile("/tmp/GeoLite2-Country.tar.gz", url); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := ExtractDatabase(*data_dir, "/tmp/GeoLite2-Country.tar.gz"); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if err := os.Remove("/tmp/GeoLite2-Country.tar.gz"); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func DownloadFile(filepath string, url string) error {
+	// Get the data
+	Debug(fmt.Sprintf("Downloading %s", url))
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+// Extract the database from the tar.gz
+func ExtractDatabase(dst string, targz string) error {
+	Debug(fmt.Sprintf("Opening %s", targz))
+	re, _ := regexp.Compile(`GeoLite2\-Country\.mmdb$`)
+
+	r, err := os.Open(targz)
+	if err != nil {
+		return err
+	}
+
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+
+		// if no more files are found return
+		case err == io.EOF:
+			return nil
+
+		// return any other error
+		case err != nil:
+			return err
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(dst, header.Name)
+
+		// check the file type
+		switch header.Typeflag {
+
+		case tar.TypeReg:
+
+			if re.Match([]byte(target)) {
+				outfile := filepath.Join(dst, "GeoLite2-Country.mmdb")
+
+				f, err := os.OpenFile(outfile, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+				if err != nil {
+					return err
+				}
+
+				Debug(fmt.Sprintf("Copy GeoLite2-Country.mmdb to %s", outfile))
+				if _, err := io.Copy(f, tr); err != nil {
+					return err
+				}
+
+				f.Close()
+			}
+		}
+	}
 }
 
 /**
  * Print the help function
  */
 var Usage = func() {
-    fmt.Fprintf(os.Stderr, "Usage: %s [-i|-c] <ipaddress|hostname>\n", os.Args[0])
-    flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr, "Usage: %s [-i] [-c] [-d <database directory>] <ipaddress|hostname|update>\n", os.Args[0])
+	flag.PrintDefaults()
 }
 
 /**
  * Return debug information `-v`
  */
 func Debug(m string) {
-    if *verbose {
-        fmt.Println(m)
-    }
+	if *verbose {
+		fmt.Println(m)
+	}
 }
